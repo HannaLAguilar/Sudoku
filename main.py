d1b5be617b585140f78879659df00d5282725f40
Hanna Lizarzaburu Aguilar <hanna.lizarzaburu@gmail.com>
2020-12-18 23:32:21 +0100

    update sudoku

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sudoku_solver
 
 
 # Image preprocessing
-img_path = 'imgs/8.jpg'
+img_path = 'imgs/4.jpg'
 img_original = cv2.imread(img_path)[:, :, ::-1]
 width, height = 450, 450
 img = cv2.resize(img_original, (width, height))
@@ -27,7 +27,7 @@ img_warp_gray = cv2.cvtColor(img_warp, cv2.COLOR_RGB2GRAY)
 
 # Sudoku digits
 digits_box = split_box_numbers(img_warp_gray)
-sudoku = get_numbers(digits_box, Classifier(), 'classifier_digit.pt')
+sudoku = get_numbers(digits_box, Classifier(), 'classifier_digit2.pt')
 sudoku_unsolving = sudoku.copy()
 
 # Sudoku solve
@@ -38,7 +38,9 @@ l1 = [ii for ii in (sudoku-sudoku_unsolving).ravel()]
 
 # Visualization
 img_stack = ([img, img_threshold, img_contours, img_points],
-             [img_warp_gray, displayNumbers(np.zeros_like(img), l0), displayNumbers(np.zeros_like(img), l1, color=(0,255,0)), np.zeros_like(img)])
+             [img_warp_gray, img_numbers(np.zeros_like(img), l0),
+              img_numbers(np.zeros_like(img), l1, color=(0, 255, 0)),
+              img_numbers(img_warp, l1, color=(0, 255, 0))])
 img_stack = stackImages(img_stack, 0.7)[:, :, ::-1]
 cv2.imshow('Stacked Images', img_stack)
 cv2.waitKey(0)
--- utils.py
+++ utils.py
@@ -116,15 +116,13 @@ def get_numbers(numbers_box, model_cnn, model_state):
     return numbers
 
 
-def displayNumbers(img,numbers,color = (255,0,0)):
-    secW = int(img.shape[1]/9)
-    secH = int(img.shape[0]/9)
-    for x in range (0,9):
-        for y in range (0,9):
-            if numbers[(y*9)+x] != 0 :
-                 cv2.putText(img, str(numbers[(y*9)+x]),
-                               (x*secW+int(secW/2)-10, int((y+0.8)*secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                            2, color, 2, cv2.LINE_AA)
+def img_numbers(img, numbers, color=(0, 255, 0)):
+    width = int(img.shape[0] / 9)
+    for x in range(0, 9):
+        for y in range(0, 9):
+            if numbers[(y * 9) + x] != 0:
+                cv2.putText(img, str(numbers[(y * 9) + x]), (x * width + int(width / 2) - 10, int((y + 0.8) * width)),
+                            cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=2, color=color, thickness=2, lineType=cv2.LINE_AA)
     return img
 
 
